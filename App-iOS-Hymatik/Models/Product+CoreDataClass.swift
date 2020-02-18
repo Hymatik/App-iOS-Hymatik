2ca22716a500c36f992a6504a2110b58620ee598
Glenn Drescher <GlennD@me.com>
2020-02-18 10:28:34 +0100

    generate Product Entity

---
+++ App-iOS-Hymatik/Models/Product+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Product+CoreDataClass.swift
+//  
+//
+//  Created by Glenn Drescher on 18/02/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Product)
+public class Product: NSManagedObject {
+
+}
