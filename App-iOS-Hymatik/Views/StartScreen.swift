6832f1ffaa1b1fb3a287251ede497f2b70c21410
Glenn Drescher <GlennD@me.com>
2020-02-19 11:42:06 +0100

    Create Menu

---
+++ App-iOS-Hymatik/Views/StartScreen.swift
@@ -0,0 +1,47 @@
+//
+//  StartScreen.swift
+//  App-iOS-Hymatik
+//
+//  Created by Glenn Drescher on 19/02/2020.
+//  Copyright © 2020 Hymatik. All rights reserved.
+//
+
+import SwiftUI
+
+struct StartScreen: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Logo()
+        
+                List {
+                    Text("Ordre")
+                    Text("Ordre historik")
+                    NavigationLink(destination: ShowScanner()) {
+                        Text("Scan Barcode")
+                    }
+                    Text("Kunde information")
+                }
+            }
+        .navigationBarTitle("Menu")
+        }
+        
+    }
+}
+
+struct Logo: View {
+    var body: some View {
+        Image("Hymatic-secondary-slogan-2500")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        .padding()
+        
+    }
+}
+
+struct StartScreen_Previews: PreviewProvider {
+    static var previews: some View {
+            StartScreen()
+
+    }
+}
