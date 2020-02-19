0699f20d479ffea8b7de6d3e3b984eb3f457730d
Glenn Drescher <GlennD@me.com>
2020-02-19 09:23:49 +0100

    Scanner restructured

--- App-iOS-Hymatik/Controller/Scanner.swift
+++ App-iOS-Hymatik/Controller/Scanner.swift
@@ -11,20 +11,23 @@ import UIKit
 
 class Scanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
+    // Those types are the barcodes the video is checked for.
     let supportedBarcodeTypes: [AVMetadataObject.ObjectType] = [.ean8, .ean13, .pdf417, .upce, .code128]
     
     
     var captureSession: AVCaptureSession!
-    var previewLayer: AVCaptureVideoPreviewLayer!
     
     //Saves found barcodes as products in the database.
     //Used in found()
     private let datahandler = Datahandler()
     
 
+    
+    // MARK: View Handler
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
 
         captureSession = AVCaptureSession()
 
@@ -34,21 +37,48 @@ class Scanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         captureSession.startRunning()
     }
     
-    func previewLayers() {
-        // Background Layer
-        view.backgroundColor = UIColor.white
-        
-        // Video Layer
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = view.layer.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        
-        
-        // GUI Layer
-        previewLayer.addSublayer(displayGuide().layer)
+    // Will start the camera and capturing when the view is shown
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
     }
     
+    // Stops the camera when the view disappears.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+
+        }
+    }
+    
+    // MARK: Barcode handling
+    
+    
+    // When a barcode is found, it will show a alert box so the user can choose to either save the product or not.
+    func found(code: String) {
+        let alert = UIAlertController(title: "Barcode: \"\(code)\" fundet.", message: "Vil du tilføj produktet til din ordre?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ja, tak.", style: .default, handler: { (action) in
+            self.datahandler.saveProduct(code: code)
+            self.captureSession.startRunning()
+        }))
+        alert.addAction(UIAlertAction(title: "Nej, ikke tilføj den.", style: .default, handler: { (action) in
+            self.captureSession.startRunning()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // When it failes to start the camera up
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
     
     // Checks the video for barcodes
     func checkVideo() {
@@ -82,6 +112,38 @@ class Scanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         }
     }
     
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+            
+        }
+        
+    }
+    
+    // MARK: View Layers
+    
+    
+    func previewLayers() {
+        var previewLayer: AVCaptureVideoPreviewLayer!
+        
+        // Background Layer
+        view.backgroundColor = UIColor.white
+        
+        // Video Layer
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        
+        // GUI Layer
+        previewLayer.addSublayer(displayGuide().layer)
+    }
     
     // Adding a Rectangle to show a guide.
     func displayGuide() -> UIImageView {
@@ -99,61 +161,7 @@ class Scanner: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         return myView
     }
 
-    // When it failes to start the camera up
-    func failed() {
-        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
-        captureSession = nil
-    }
 
-    
-    // Will start the camera and capturing when the view is shown
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        if (captureSession?.isRunning == false) {
-            captureSession.startRunning()
-        }
-    }
-
-    // Stopes the camera when the view disappears.
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        if (captureSession?.isRunning == true) {
-            captureSession.stopRunning()
-
-        }
-    }
-
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        captureSession.stopRunning()
-
-        if let metadataObject = metadataObjects.first {
-            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-            guard let stringValue = readableObject.stringValue else { return }
-            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-            found(code: stringValue)
-            
-        }
-        
-    }
-
-    
-    // When a barcode is found, it will show a alert box so the user can choose to either save the product or not.
-    func found(code: String) {
-        let alert = UIAlertController(title: "Barcode: \"\(code)\" fundet.", message: "Vil du tilføj produktet til din ordre?", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ja, tak.", style: .default, handler: { (action) in
-            self.datahandler.saveProduct(code: code)
-            self.captureSession.startRunning()
-        }))
-        alert.addAction(UIAlertAction(title: "Nej, ikke tilføj den.", style: .default, handler: { (action) in
-            self.captureSession.startRunning()
-        }))
-        self.present(alert, animated: true, completion: nil)
-    }
-    
     
     // MARK: View settings
     
